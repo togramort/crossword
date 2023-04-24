3037c4001f8a59a26bcdaca7ddab04a8c51407c5
togramort <togramort@gmail.com>
2023-04-24 04:26:25 +0300

    implementing algo

---
+++ login.js
@@ -0,0 +1,28 @@
+// получаем модальное окно и кнопку закрытия
+var modal = document.getElementById("loginModal");
+var closeBtn = document.getElementsByClassName("close")[0];
+
+// функция открытия модального окна
+function showLogin() {
+  modal.style.display = "block";
+}
+
+// функция закрытия модального окна
+function closeLoginModal() {
+  modal.style.display = "none";
+}
+
+// функция проверки логина и пароля
+function validateLogin() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  // здесь должна быть проверка логина и пароля на сервере
+  if (username == "admin" && password == "password") {
+    window.open("generator.html", "_self");
+    return false;
+  } else {
+    alert("incorrect login or password ;(");
+    return false;
+  }
+}
