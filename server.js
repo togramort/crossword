c820cb37a72ae7c7e3464f549b621607aced3509
togramort <togramort@gmail.com>
2023-04-25 22:33:57 +0300

    for deployment

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const http = require('http');
 const fs = require('fs');
 const mongoose = require('mongoose');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 const url = 'mongodb+srv://coolusername:greatpassword@crossword.vmrxejk.mongodb.net/?retryWrites=true&w=majority';
 
 const server = http.createServer((req, res) => {
     console.log('server request');
     res.setHeader('content-type', 'text/html');
-    fs.readFile('login.html', (error, data) => {
+    fs.readFile('index.html', (error, data) => {
         error ? res.write("oops: ", error) : res.write(data);
         res.end();
     });
