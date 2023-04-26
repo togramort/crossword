1711fa092df6001a1f15d7978eb53ed8eb1acd9a
togramort <togramort@gmail.com>
2023-04-26 21:48:01 +0300

    all over again

--- dev/login.js
+++ dev/login.js
@@ -1,5 +1,3 @@
-const { NODE_URL } = require("../server");
-
 // получаем модальное окно и кнопку закрытия
 var modal = document.getElementById("loginModal");
 var closeBtn = document.getElementsByClassName("close")[0];
@@ -19,29 +17,6 @@ function closeLoginModal() {
 
 
 // функция проверки логина и пароля
-// const form = document.querySelector('form');
-const form = document.getElementById('loginForm');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-  fetch('/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ username, password })
-  }).then(response => {
-    console.log(process.env)
-    window.location.href = `${NODE_URL}/generator`;
-    console.log(response);
-    if (!response.ok) {
-      throw new Error('server error');
-    }
-  }).catch(error => {
-    error ? console.log("oops: ", error) : console.log("all cool");
-  })
-});
 
 
 function openCrossword() {
